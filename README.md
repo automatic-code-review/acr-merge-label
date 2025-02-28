3d85b88c93704036367239eb02c00867e97321ba
kielsonzinn <kielsonzinn@gmail.com>
2025-02-28 10:24:43 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
-# acr-extension-name
+# acr-merge-label
 
 Arquivo config.json
 
+- configs
+    - description: Descrição do comentario que deve ser adicionado
+    - labels: Labels que precisam estar presente
+    - authors: Autores que ira verificar se tem a label
+    - branches: Branches que ira verificar se tem a label, sendo uma lista de regex
+
 ```json
 {
-
+  "stage": "static",
+  "language": "PYTHON",
+  "configs": [
+    {
+      "description": "${LABEL}",
+      "labels": [
+        ""
+      ],
+      "authors": [
+        ""
+      ]
+    },
+    {
+      "description": "${LABEL}",
+      "labels": [
+        ""
+      ],
+      "branches": [
+        ""
+      ]
+    }
+  ]
 }
+
 ```
--- src/review.py
+++ src/review.py
@@ -1,25 +1,44 @@
+import re
+
 import automatic_code_review_commons as commons
 
-def review(config):
-    path_target = config['path_target']
-    path_source = config['path_source']
 
+def review(config):
     merge = config['merge']
-    project_id = merge['project_id']
-    merge_request_id = merge['merge_request_id']
-    
+    configs = config['configs']
+    labels_in_merge = merge['labels']
+    branch = merge['branch']['target']
+
     comments = []
-    
-    # TODO IMPLEMENTAR EXTENSION
-    #  O OBJETO DE COMENTARIO DEVE POSSUIR O SEGUINTE FORMATO
-    #  commons.comment_create(
-    #     comment_id=commons.comment_generate_id( "" ),
-    #     comment_path="",
-    #     comment_description="",
-    #     comment_snipset=True,
-    #     comment_end_line=1,
-    #     comment_start_line=1,
-    #     comment_language="",
-    # )
+
+    for config_obj in configs:
+        if "authors" in config_obj and merge['author'] not in config_obj["authors"]:
+            continue
+
+        if "branches" in config_obj and not __verify_regex_list(config_obj['branches'], branch):
+            continue
+
+        labels = config_obj['labels']
+
+        for label in labels:
+            if label not in labels_in_merge:
+                description = config_obj['description'].replace("${LABEL}", label)
+
+                comments.append(commons.comment_create(
+                    comment_id=commons.comment_generate_id(description),
+                    comment_path=None,
+                    comment_description=description,
+                    comment_snipset=False,
+                ))
+
+    print(comments)
 
     return comments
+
+
+def __verify_regex_list(regex_list, branch):
+    for regex in regex_list:
+        if re.match(regex, branch):
+            return True
+
+    return False
